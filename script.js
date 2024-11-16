03b5cabb19e864e46a088a56cd6b5b13b60a891f
ARPAN SAHA <arpansaha004@gmail.com>
2024-11-16 13:08:29 +0530

    update the content, changed text and color , shadow to the navigator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// smooth scrolling feature 
+// ! smooth scrolling feature 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -8,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Scroll-to-Top Button feature
+// ! Scroll-to-Top Button feature
 const scrollToTopBtn = document.createElement('button');
 scrollToTopBtn.innerHTML = '↑';
 scrollToTopBtn.style.position = 'fixed';
@@ -22,6 +22,8 @@ scrollToTopBtn.style.borderRadius = '50%';
 scrollToTopBtn.style.padding = '10px 15px';
 scrollToTopBtn.style.cursor = 'pointer';
 scrollToTopBtn.style.display = 'none'; // Initially hidden
+scrollToTopBtn.style.boxShadow =
+    '0px 0px 15px black';
 document.body.appendChild(scrollToTopBtn);
 
 window.addEventListener('scroll', function () {
